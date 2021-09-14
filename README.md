7af8b4208e5040c8ad6848175af90cbaafccf981
clemence goh <clemencegoh@gmail.com>
2021-09-14 15:37:57 +0800

    better description for gogen ewplace

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Generation tool for go MVC
 
 ## Example usage:
+
 - `gogen new --component Test`
 - `gogen new -c Test`
 - `gogen replace -f webpack.config.js -w localhost -r <ip>`
 - `gogen replace -f webpack.config.js -r localhost -w <ip>`
+
+# Creating or adding on to gogen
+
+- after changing the files, use `go install` to rebuild and add to path
--- cmd/replace.go
+++ cmd/replace.go
@@ -31,13 +31,8 @@ var (
 // replaceCmd represents the replace command
 var replaceCmd = &cobra.Command{
 	Use:   "replace",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Replaces word 'w' with word 'r'",
+	Long:  `See flags for replacing words w with word r in file f`,
 	Run: func(cmd *cobra.Command, args []string) {
 		CheckFlag(cmd, filename)
 		CheckFlag(cmd, word)
